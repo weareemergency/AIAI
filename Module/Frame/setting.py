07d9c711c9cb251ad247639fa02f8256374626dc
soft0725 <a01021146076@gmail.com>
2023-07-19 00:56:44 +0900

    FIX :: frame_setting

--- Module/Frame/setting.py
+++ Module/Frame/setting.py
@@ -1,15 +1,11 @@
 import cv2
 
-def frame_setting(frame, face_mesh):
-    frame.flags.writeable = False  # 픽셀값 수정 불가능
+
+def frame_setting(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = cv2.flip(frame, 1)
-    results = face_mesh.process(frame)
 
-    frame.flags.writeable = True
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    return frame
 
-    return frame, results
 
 def get_shape(width, height):
     width = width
