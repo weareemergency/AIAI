c200d18aada2eeb9b26068523bbad24868d09bd0
soft0725 <a01021146076@gmail.com>
2023-07-19 16:42:10 +0900

    FIX :: download detect image

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 import mediapipe as mp
 import threading
-import time
 
 from Module.Frame.setting import frame_setting, get_shape
+from Module.Frame.guide import UserGuide
 from Module.Draw.draw import Draw
 from Module.Draw.XY import Vertex, Body
 
@@ -16,28 +16,20 @@ def main():
 
     first_rect = 400
     second_rect = 350
+    count = 0
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        # --------
-        step1_img = cv2.imread('static/image/step1.png')
-        step2_img = cv2.imread('static/image/step2.png')
-        step3_img = cv2.imread('static/image/step3.png')
+        guide = UserGuide(frame)
+        x, y, overlay_width, overlay_height = guide.position()
 
-        x = 650
-        y = 1
-
-        # 이미지 크기 조정
-        overlay_width = 650
-        overlay_height = 130
-        step1 = cv2.resize(step1_img, (overlay_width, overlay_height))
-        step2 = cv2.resize(step2_img, (overlay_width, overlay_height))
-        step3 = cv2.resize(step3_img, (overlay_width, overlay_height))
-
-        # ------------
+        step1 = guide.step1()
+        step2 = guide.step2()
+        step3 = guide.step3()
+        origin_frame = frame.copy()
 
         frame_rgb = frame_setting(frame)
         results = pose.process(frame_rgb)
@@ -63,18 +55,6 @@ def main():
             right_shoulder_x, right_shoulder_y = right_shoulder.body_xy()
             left_shoulder_x, left_shoulder_y = left_shoulder.body_xy()
 
-            right_ear = Draw(frame, width, height)
-            left_ear = Draw(frame, width, height)
-            nose = Draw(frame, width, height)
-            right_shoulder = Draw(frame, width, height)
-            left_shoulder = Draw(frame, width, height)
-
-            right_ear.body_circle(r_ear_x, r_ear_y)
-            left_ear.body_circle(l_ear_x, l_ear_y)
-            nose.body_circle(nose_x, nose_y)
-            right_shoulder.body_circle(right_shoulder_x, right_shoulder_y)
-            left_shoulder.body_circle(left_shoulder_x, left_shoulder_y)
-
             center = Draw(frame, width, height)
 
             ear_diff_x = l_ear_x - r_ear_x
@@ -85,11 +65,16 @@ def main():
             ifin_3 = (x1 <= right_shoulder_x <= x2) and (y1 <= right_shoulder_y <= y2) and (x1 <= left_shoulder_x <= x2) and (y1 <= left_shoulder_y <= y2)
 
             if ifin_1 and ifin_2 and ifin_3:
-                if (abs(ear_diff_x) - abs(ear_diff_y)) < 32:
-                    print(abs(ear_diff_x) - abs(ear_diff_y))
+                if (abs(ear_diff_x) - abs(ear_diff_y)) < 50:
                     frame[y:y + overlay_height, x:x + overlay_width] = step3
                     center.center_rect(first_rect, 1)
                     center.center_rect(second_rect, 1)
+                    count += 1
+                    if count == 40:
+                        cv2.imwrite('Result/result.jpeg', origin_frame)
+
+                    if cv2.waitKey(1) == 27 or count == 60:
+                        break
                 else:
                     frame[y:y + overlay_height, x:x + overlay_width] = step2
                     center.center_rect(first_rect, 1)
@@ -98,10 +83,6 @@ def main():
                 frame[y:y + overlay_height, x:x + overlay_width] = step1
                 center.center_rect(first_rect, 0)
                 center.center_rect(second_rect, 0)
-
-            nose.body_circle(nose_x, nose_y)
-            right_ear.body_circle(r_ear_x, r_ear_y)
-            left_ear.body_circle(l_ear_x, l_ear_y)
 
         cv2.imshow('Main', frame)
 
@@ -128,4 +109,3 @@ if __name__ == "__main__":
 
     main_thread = threading.Thread(target=main())
     main_thread.start()
-
